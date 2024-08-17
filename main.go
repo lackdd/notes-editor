2549164753df0738f94a119f9cf59c1157efbd62
ralfrandver <ralf.randver@gmail.com>
2024-08-17 14:52:21 +0000

    Update 'main.go'

--- main.go
+++ main.go
@@ -9,3 +9,26 @@ func main() {
 	fmt.Println("hello")
 	fmt.Println(notes.Test())
 }
+package sprint
+
+import (
+	"fmt"
+	"strings"
+	"bufio"
+	"os"
+)
+
+func AddANote(string) error {
+    file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644)
+   
+    defer file.Close()
+	
+	reader := bufio.NewReader(os.Stdin)
+    input, err := reader.ReadString('\n')
+   
+    input = strings.TrimSpace(input)
+    _, err = file.WriteString(input + "\n")
+   
+    fmt.Println("Text successfully added to the file!")
+    return nil
+}
